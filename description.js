ae1a56ed9318c819e938a250061dd6871dfe95a9
Francesco-ovvio <80890507+Francesco-ovvio@users.noreply.github.com>
2021-04-03 20:23:01 +0200

    Add files via upload

---
+++ description.js
@@ -0,0 +1,8 @@
+function showDesc(){
+    var x = document.getElementById("descrizione");
+    if(x.style.display === "none"){
+        x.style.display = "block"; 
+    }else{
+        x.style.display = "none";
+    }
+}
