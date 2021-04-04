9e9fbd30e4ed585f9621587b640b021f4caf12ec
Francesco-ovvio <80890507+Francesco-ovvio@users.noreply.github.com>
2021-04-04 18:46:18 +0200

    Add files via upload

--- description.js
+++ description.js
@@ -6,3 +6,22 @@ function showDesc(id){
         x.style.display = "none";
     }
 }
+
+function search(){
+    var input, filter, prodGrid, prod, h1, i, txtValue;
+    input = document.getElementById('searchBar');
+    filter = input.value.toUpperCase();
+    prodGrid = document.getElementById('product-grid');
+    prod = prodGrid.getElementsByClassName('product');
+
+    for(i = 0; i<prod.length; i++){
+        h1 = prod[i].getElementsByTagName('h1')[0];
+        txtValue = h1.innerText;
+        console.log(filter);
+        if(txtValue.toUpperCase().indexOf(filter) > -1) {
+            prod[i].style.display = "";
+        } else {
+            prod[i].style.display = "none";
+        }
+    }
+}
