3f0db8683e80eb7bf9371d5438dfc93de15716df
Francesco-ovvio <80890507+Francesco-ovvio@users.noreply.github.com>
2021-04-07 20:27:55 +0200

    Delete content.js

--- content.js
+++
@@ -1,4 +0,0 @@
-const titoli = ["Orzo", "Frumento", "Granturco"];
-const immagini = ['https://www.greenme.it/wp-content/uploads/2017/01/orzo_cover-1200x720.jpg', 'https://massimospattini.com/wp-content/uploads/2015/09/Fotolia_85759700_S.jpg', 'https://www.focus.it/site_stored/imgs/0001/007/200271611724_16.630x360.jpg', ];
-const prezzi = ['10', '20', '30'];
-const descrizione = ['questo è orzo', 'questo è frumento', 'questo è granturco'];
