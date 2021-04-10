9b554c0646ed4b76b6e7632f7e3a29e21380f9da
Francesco-ovvio <80890507+Francesco-ovvio@users.noreply.github.com>
2021-04-10 09:22:33 +0200

    Add files via upload

---
+++ content.js
@@ -0,0 +1,19 @@
+const titoli = ["Orzo", "Frumento", "Granturco","Riso", "Ricotta", "Pepato fresco", "Pepato stagionato", "Provola"];
+const immagini = ['https://i.imgur.com/LIYpK4U.jpg',
+                'https://i.imgur.com/rzCB39C.jpg',
+                'https://i.imgur.com/YN7ZKIO.jpg',
+                'https://i.imgur.com/s7TzyB8.jpg',
+                'https://i.imgur.com/SfvOSUk.jpg',
+                'https://i.imgur.com/rYJ3wZ5.jpg',
+                'https://i.imgur.com/evyP8Ia.jpg',
+                'https://i.imgur.com/Aw243VD.jpg'];
+const descrizione = ["L’orzo comune è la specie economicamente più importante tra quelle coltivate del genere Hordeum, quella da cui si ricava l'orzo alimentare da cui dipende una considerevole parte dell'alimentazione mondiale.",
+    "Il grano o frumento, arcaicamente anche trittico, è un genere della famiglia graminacee, cereale di antica coltura, la cui area d'origine è localizzata tra Mar Mediterraneo, Mar Nero e Mar Caspio.",
+    "Il mais è una pianta erbacea annuale della famiglia delle Poaceae, tribù delle Maydeae: addomesticato dalle popolazioni indigene in Messico centrale in tempi preistorici circa 10.000 anni fa.",
+    "Il riso o risoide è un alimento costituito dalla cariosside prodotta da diverse piante dei generi Oryza e Zizania, opportunamente lavorata. Le più note specie utilizzate sono l'Oryza sativa e l'Oryza glaberrima.",
+    "La ricotta (dal latino recocta) è un prodotto caseario, più precisamente un latticino. La ricotta, pur essendo un prodotto caseario, non è, per legge, formaggio ma va classificata semplicemente come latticino: non viene ottenuta infatti attraverso la coagulazione della caseina, ma delle proteine del siero di latte, cioè la parte liquida che si separa dalla cagliata durante la caseificazione.",
+    "Per primosale, o primo sale, s'intende un determinato grado di stagionatura del formaggio pecorino, ma il termine indica anche un formaggio pecorino tipico siciliano. Il termine 'primosale' è usato soprattutto nell'Italia meridionale e in Sicilia è una denominazione che è stata inserita nella lista dei prodotti agroalimentari tradizionali italiani (P.A.T.) del Ministero delle politiche agricole alimentari e forestali, essendo stato riconosciuto come prodotto tipico siciliano.",
+    "Con secondo sale, o secondosale, s'intende un determinato grado di stagionatura del formaggio pecorino, ma il termine indica anche un formaggio pecorino tipico siciliano. La dizione 'secondo sale' è usata soprattutto nell'Italia meridionale mentre in Sicilia è una denominazione che è stata inserita nella lista dei prodotti agroalimentari tradizionali italiani (P.A.T) del Ministero delle politiche agricole alimentari e forestali (Mipaaf), essendo stato riconosciuto come prodotto tipico siciliano.",
+    "La provola è un formaggio di latte vaccino, a latte crudo e a pasta cotta o pasta semicotta e filata. In genere ha la forma di una sfera schiacciata, dal peso di circa mezzo chilo. Una provola ottenuta da latte bufalino viene prodotta in Campania."];
+
+const prova = ["ciao", "prova"];
---
+++ sidenav.js
@@ -0,0 +1,7 @@
+function openNav(){
+    document.getElementById("MySidenav").style.width = "250px";
+}
+
+function closeNav(){
+    document.getElementById("MySidenav").style.width = "0px";
+}
